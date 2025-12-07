bf683f7978e2ab78f1c2d17e63471c8cab7a7cc5
neil <neilganguly2007@gmail.com>
2025-12-07 18:04:28 +0100

    Import Issues

--- LocalLoop/src/config/firebase.ts
+++ LocalLoop/src/config/firebase.ts
@@ -3,16 +3,16 @@
 // Get these from: https://console.firebase.google.com -> Project Settings -> Your apps
 
 import { initializeApp } from 'firebase/app';
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth/react-native';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
     apiKey: "AIzaSyBWBc4M-v8F744Fq8ptOeGDY5Ws0kK1VKk",
-    authDomain: "localloop-auth.firebaseapp.com",
+    authDomain: "localloop-42221.firebaseapp.com",
     projectId: "localloop-auth",
-    storageBucket: "localloop-auth.appspot.com",
+    storageBucket: "localloop.appspot.com",
     messagingSenderId: "171259286074",
     appId: "1:171259286074:web:86d08e49c8983e478adb03"
 };

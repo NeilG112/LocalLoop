2fc1ce335fe64450c7a20a267995d086075546c1
neil <neilganguly2007@gmail.com>
2025-12-07 15:11:22 +0100

    test

--- LocalLoop/app/index.tsx
+++ LocalLoop/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>This is just a test for now.</Text>
     </View>
   );
 }
